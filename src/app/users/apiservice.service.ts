0d6d232fe34e85d7c91bd3cae60414b3e39887c4
it18177138 <gimhan.thebuwana99@gmail.com>
2022-03-24 16:58:34 +0530

    dfgnhngh

--- src/app/users/apiservice.service.ts
+++ src/app/users/apiservice.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiserviceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  
+  apiUrl = 'http://localhost:3000/api';
+
+  signup(data:any):Observable<any>
+  {
+    return this.http.post('${this.apiUrl}/signup ,data');
+  }
+
+
 }
