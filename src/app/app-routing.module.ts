bb94508a457f48367e2db241410624b4bd56669b
it18177138 <gimhan.thebuwana99@gmail.com>
2022-03-25 22:04:33 +0530

    sign up new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './users/login/login.component';
-import { SignupComponent } from './users/signup/signup.component';
-import { TutorialComponent } from './users/tutorial/tutorial.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'tutorial',component:TutorialComponent}
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login', component:LoginComponent},
+  {path:'signup', component:SignupComponent},
+  // {path:'dashboard',component:Dash}
+  
 ];
 
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiserviceService} from './users/apiservice.service';
-import {Router} from '@angular/router';
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'bill-detection-system';
-  showmenu = false;
-  constructor(private service:ApiserviceService,private router:Router){}
-  token = this.service.getToken();
-  name = localStorage.getItem('username');;
   
-  
-  
-
-   ngOnInit(): void {
+  ngOnInit(): void {
     
-   if(this.token)
-    {
-      this.showmenu = true;
-    }else
-    {
-      this.showmenu = false;
-    }
+   
   }
 
-  logout()
-  {
-    console.log('logout##');
-    
-    localStorage.clear();
-    this.router.navigate(['login']).then((){
-      window.location.reload();
-    });
-  }
+ 
  
 }
