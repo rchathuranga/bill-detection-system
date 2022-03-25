b9c4a3bf3ac0b1c9cb3e56b2599181dea6dd0da6
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2022-03-26 00:43:07 +0530

    Complete File Upload Front end part

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./main/main.component";
+import {ResultComponent} from "./result/result.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: MainComponent
+  },
+  {
+    path:'main',
+    component: MainComponent
+  },
+  {
+    path:'result',
+    component: ResultComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { DragDropFileUploadDirective } from './drag-drop-file-upload.directive';
+import {ReactiveFormsModule} from "@angular/forms";
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DragDropFileUploadDirective,
+    ResultComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule
   ],
